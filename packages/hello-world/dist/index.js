77d57501dcb59b36f5f4a3fe56cead17275d655e
Thomaz Prates <thomazdcprates@gmail.com>
2025-11-03 13:31:06 -0300

    Initial Lerna project

---
+++ packages/hello-world/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = helloWorld;
+function helloWorld() {
+    console.log("Hello, world!");
+    return "Hello, world!";
+}
---
+++ packages/hello-world/src/index.ts
@@ -0,0 +1,4 @@
+export function helloWorld(): string {
+  console.log("Hello, world!");
+  return "Hello, world!";
+}
