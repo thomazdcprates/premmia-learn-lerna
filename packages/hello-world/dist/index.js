1d0e65004d2b9099ad25bf02d5ad2d78cb6647d7
Thomaz Prates <thomazdcprates@gmail.com>
2025-11-04 16:48:07 -0300

    thirty commit

--- packages/hello-world/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = helloWorld;
-function helloWorld() {
-    console.log("Hello, worldsssss!asdasss");
-    return "Hello, worldsssss!ssasdas";
-}
