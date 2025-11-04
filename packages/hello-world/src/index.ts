87eddb1c274e9cad81b124fe56e9a793a7581faa
Thomaz Prates <thomazdcprates@gmail.com>
2025-11-04 16:41:34 -0300

    thirty commit

--- packages/hello-world/src/index.ts
+++ packages/hello-world/src/index.ts
@@ -1,4 +1,4 @@
 export function helloWorld(): string {
-  console.log("Hello, worldsssss!");
-  return "Hello, worldsssss!";
+  console.log("Hello, worldsssss!ss");
+  return "Hello, worldsssss!sss";
 }
