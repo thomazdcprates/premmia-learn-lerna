ceea2f133d832d80c5a6a81e5dd0a19e3671a62b
Thomaz Prates <thomazdcprates@gmail.com>
2025-11-12 13:27:39 -0300

    create new package dynamodb

--- packages/dynamodb/src/index.ts
+++ packages/dynamodb/src/index.ts
@@ -1,14 +1,19 @@
-import { ScanCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import {
+  ScanCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamodbConfig } from "./interfaces";
+import { InternalError } from "./errors";
 
 class Dynamodb {
   protected client: DynamoDBDocumentClient;
 
-  constructor(region: string, endpoint: string) {
-    const dynamoDBClient = new DynamoDBClient({
-      endpoint,
-      region,
-    });
+  constructor(config: DynamodbConfig) {
+    const dynamoDBClient = new DynamoDBClient(config);
 
     this.client = DynamoDBDocumentClient.from(dynamoDBClient);
   }
@@ -19,6 +24,56 @@ class Dynamodb {
       const result = await this.client.send(command);
       return result.Items ?? [];
     } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalError(error.message);
+      }
+
+      throw error;
+    }
+  }
+
+  async getByKey(tableName: string, key: any) {
+    try {
+      const command = new GetCommand({ TableName: tableName, Key: key });
+      const result = await this.client.send(command);
+      return result.Item;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalError(error.message);
+      }
+
+      throw error;
+    }
+  }
+
+  async save(tableName: string, item: any) {
+    try {
+      const command = new PutCommand({
+        TableName: tableName,
+        Item: item,
+      });
+
+      await this.client.send(command);
+
+      return item;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalError(error.message);
+      }
+
+      throw error;
+    }
+  }
+
+  async delete(tableName: string, key: any) {
+    try {
+      const command = new DeleteCommand({ TableName: tableName, Key: key });
+      await this.client.send(command);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalError(error.message);
+      }
+
       throw error;
     }
   }
