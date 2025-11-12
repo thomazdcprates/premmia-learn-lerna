aac952d66d2ca4b6c9a5ec983a943832a45d0760
Thomaz Prates <thomazdcprates@gmail.com>
2025-11-12 09:33:01 -0300

    create new package bye world

--- packages/hello-world/src/index.ts
+++ packages/hello-world/src/index.ts
@@ -1,4 +1,4 @@
 export function helloWorld(): string {
-  console.log("Hello, worldsssss!asdasss");
-  return "Hello, worldsssss!ssasdas";
+  console.log("Hello, world!");
+  return "Hello, world!";
 }
